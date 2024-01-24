89946dc765d80804c9b80acb2ead84e863899fdb
Marie-Michelle Schneider <udixi@student.kit.edu>
2024-01-24 15:04:47 +0100

    implemented ErrorWindow

---
+++ src/view/GUI/Visuals/ErrorWindow.py
@@ -0,0 +1,15 @@
+from PyQt5.QtWidgets import QMessageBox
+
+class ErrorWindow(QMessageBox):
+
+    WINDOW_TITLE = "Error"
+
+    def __init__(self, error_message):
+        super().__init__()
+        # Setup ErrorWindow
+        self.setText(error_message)
+        self.setIcon(QMessageBox.Critical)
+        self.setWindowTitle(self.WINDOW_TITLE)
+        self.addButton(QMessageBox.Ok)
+        # Show immediately
+        self.exec_()
