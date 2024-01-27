3c0065f5d875ae5e2788226b802ca7b0675ac20e
Caspar Carl-Friedrich von Heusinger <uvhuj@student.kit.edu>
2024-01-27 13:39:05 +0100

    Resolve "DataFetcher (Abstract)"

--- src/fetcher/FetcherInterface.py
+++ src/fetcher/FetcherInterface.py
@@ -3,6 +3,6 @@ from typing import Protocol
 class FetcherInterface(Protocol):
     """Interface for fetchers, instances that can make changes to the current project of a model"""
 
-    def update_project() -> bool:
+    def update_project(self) -> bool:
         """Changes the current_project of a model instance passed at creation in some way, depending on implementing class """
         raise NotImplementedError
