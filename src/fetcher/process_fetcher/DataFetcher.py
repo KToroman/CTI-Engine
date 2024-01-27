0cbace34b0387916852a645f00e3f546dedc4f6e
CasparCFvH <caspar.v.heusinger@gmail.com>
2024-01-27 17:10:36 +0100

    DataFetcher_change + PassiveDataFetcher_implementation + ProcessCollector_fix

--- src/fetcher/process_fetcher/DataFetcher.py
+++ src/fetcher/process_fetcher/DataFetcher.py
@@ -6,6 +6,7 @@ from src.fetcher.FetcherInterface import FetcherInterface
 from src.fetcher.process_fetcher.process_observer.ProcessCollector import ProcessCollector
 from src.fetcher.process_fetcher.process_observer.metrics_observer.DataObserver import DataObserver
 from src.model.Model import Model
+from src.model.core.DataEntry import DataEntry
 from src.model.core.ProcessPoint import ProcessPoint
 
 
@@ -14,7 +15,7 @@ class DataFetcher(Protocol, FetcherInterface):
     process_collector: ProcessCollector = None
     data_observer: DataObserver = None
 
-    def add_data_entry(self, process_point: ProcessPoint):
+    def add_data_entry(self, process_point: ProcessPoint) -> DataEntry:
         raise NotImplemented
 
     def fetch_metrics(self, process: psutil.Process) -> ProcessPoint:
--- src/fetcher/process_fetcher/PassiveDataFetcher.py
+++ src/fetcher/process_fetcher/PassiveDataFetcher.py
@@ -1,3 +1,6 @@
+import os.path
+from typing import List
+
 import psutil
 
 from src.fetcher.process_fetcher.DataFetcher import DataFetcher
@@ -6,27 +9,42 @@ from src.fetcher.process_fetcher.process_observer.metrics_observer.DataObserver 
 from src.model.Model import Model
 from src.model.core.DataEntry import DataEntry
 from src.model.core.ProcessPoint import ProcessPoint
+from src.model.core.Project import Project
 
 
 class PassiveDataFetcher(DataFetcher):
 
-    def __init__(self, model: Model):
+    def __init__(self, model: Model, path_to_save: str):
         self.model = model
-        self.process_collector: ProcessCollector = ProcessCollector()
+        self.process_collector: ProcessCollector = ProcessCollector(-1)
         self.data_observer: DataObserver = DataObserver()
+        self.path_to_save = path_to_save
 
+    def __check_for_project(self, process: psutil.Process) -> bool:
+        return self.process_collector.check(process)
 
-    def __check_for_project(self, process: psutil.Process):
-
-    def add_data_entry(self, process_point: ProcessPoint):
-        entry = DataEntry()
-
-
-        self.model.insert_datapoints(entry)
+    def add_data_entry(self, process_point: ProcessPoint) -> DataEntry:
+        path: str = os.path.abspath(process_point.process.cmdline()[1])
+        entry = DataEntry(path, process_point.timestamp, process_point.metrics)
+        return entry
 
     def fetch_metrics(self, process: psutil.Process) -> ProcessPoint:
-        raise NotImplemented
+        return self.data_observer.observe(process)
 
     def update_project(self) -> bool:
-        raise NotImplemented
+        processes = self.process_collector.catch_processes()
+        if processes.__len__() != 0:
+            entries: List[DataEntry] = list()
+            for proc in processes:
+                if not self.__check_for_project(proc):
+                    ppid: int = proc.ppid()
+                    parent_ppid: int = psutil.Process(ppid).ppid()
+                    working_dir: str = psutil.Process(parent_ppid).cwd()
+                    self.model.add_project(Project(working_dir, parent_ppid, self.path_to_save))
 
+                entries.append(self.add_data_entry(self.fetch_metrics(proc)))
+
+            self.model.insert_datapoints(entries)
+            return True
+
+        return False
