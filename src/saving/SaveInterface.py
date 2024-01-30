6434e9ac3e23016c89fb99d7236a44c6770aa2df
Caspar Carl-Friedrich von Heusinger <uvhuj@student.kit.edu>
2024-01-30 19:20:16 +0100

    Resolve "Implement File Saving"

---
+++ src/saving/SaveInterface.py
@@ -0,0 +1,9 @@
+from typing import Protocol
+
+from src.model.core.Project import Project
+
+
+class SaveInterface(Protocol):
+    """SaveInterface is used to save projects."""
+    def save_project(self, project: Project):
+        """Saves the given project into a file."""
