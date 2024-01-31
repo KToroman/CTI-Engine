cce21d678948c97eaa61a601f6e9ae436163fe2f
KToroman <katarina@toroman.name>
2024-01-31 13:32:31 +0100

    pretty much done, need to discuss the paths of headers etc...

--- src/builder/header_builder/FileBuilder.py
+++ src/builder/header_builder/FileBuilder.py
@@ -5,21 +5,31 @@ from pathlib import Path
 
 class FileBuilder:
     def __init__(self, compile_command: str, source_file_name: str, build_path: str) -> None:
-        self.original_compile_command: List[str] = compile_command.split(" ")
-        self.source_file_name = source_file_name
-        self.build_path = build_path
+        self.__original_compile_command: List[str] = compile_command.split(" ")
+        self.__source_file_name = source_file_name
+        self.__build_path = build_path
 
     def generate_source_file(self, header: Header) -> str:
-        path = Path(self.build_path) / 'Active_Mode_Build'
+        '''generates a source file that includes the header named header.path + .cpp
+        The files are generated in the CTIEngine-directory/Active_Mode_Build.'''
+        path = Path(self.__build_path) / 'Active_Mode_Build'
+        # will just be ignored if directory already exists
         path.mkdir(exist_ok=True)
         file_name: str = header.path + ".cpp"
         with (path / file_name).open('w') as new_source_file:
-            new_source_file.write(...)
+            new_source_file.write(self.__source_file_content(header))
+            new_source_file.close()
+        return file_name
+
+    def __source_file_content(header: Header) -> str:
+        content: str = f'#include "{header.path}"'
+        return content
 
     def get_compile_command(self, header: Header) -> str:
+        # TODO -I for project directory
         compile_command_header: str = ""
-        for entry in self.original_compile_command:
-            if entry is self.source_file_name:
+        for entry in self.__original_compile_command:
+            if entry is self.__source_file_name:
                 compile_command_header += header.path + ".cpp"
             else:
                 compile_command_header += entry
