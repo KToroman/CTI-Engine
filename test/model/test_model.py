20d49c72f65609c03ab307f984054e720780623e
CasparCFvH <caspar.v.heusinger@gmail.com>
2024-02-01 15:06:17 +0100

    more_tests_add

--- test/model/test_model.py
+++ test/model/test_model.py
@@ -14,65 +14,73 @@ from src.model.core.SourceFile import SourceFile
 
 
 class TestModel(unittest.TestCase):
+    test_project_1: Project = Project("", 123, "")
+    test_project_2: Project = Project("", 1234, "")
+    test_metrics: List[Metric] = [Metric(10, MetricName.CPU), Metric(10, MetricName.RAM)]
+    test_entry_1: DataEntry = DataEntry("test_cfile", 2, test_metrics)
+    test_entry_2: DataEntry = DataEntry("test_cfile", 3, test_metrics)
+    test_entry_list: List[DataEntry] = [test_entry_1, test_entry_2]
+    test_model: Model = Model()
 
     def test_insert_datapoints_for_existing_cfile(self):
-        self.maxDiff = None
-        test_model: Model = Model()
-        test_project: Project = Project("", 123, "")
-        test_project.get_sourcefile("test_cfile")
-        test_metrics: List[Metric] = [Metric(10, MetricName.CPU), Metric(10, MetricName.RAM)]
-        test_entry_1: DataEntry = DataEntry("test_cfile", 2, test_metrics)
-        test_entry_2: DataEntry = DataEntry("test_cfile", 3, test_metrics)
-        test_entry_list: List[DataEntry] = [test_entry_1, test_entry_2]
-        test_model.add_project(test_project)
+        self.__clear()
 
-        test_model.insert_datapoints(test_entry_list)
-        test_result: CFile = test_model.current_project.source_files[0]
+        self.test_project_1.get_sourcefile("test_cfile")
+        self.test_model.add_project(self.test_project_1)
+        self.test_model.insert_datapoints(self.test_entry_list)
+        test_result: CFile = self.test_model.current_project.source_files[0]
+
         test_cfile: CFile = SourceFile("test_cfile")
-        test_cfile.data_entries.extend(test_entry_list)
+        test_cfile.data_entries.extend(self.test_entry_list)
 
         self.assertEqual(jsonpickle.encode(test_result), jsonpickle.encode(test_cfile))
 
     def test_insert_datapoints_for_non_existing_cfile(self):
-        self.maxDiff = None
-        test_model: Model = Model()
-        test_project: Project = Project("", 123, "")
-        test_metrics: List[Metric] = [Metric(10, MetricName.CPU), Metric(10, MetricName.RAM)]
-        test_entry_1: DataEntry = DataEntry("test_cfile", 2, test_metrics)
-        test_entry_2: DataEntry = DataEntry("test_cfile", 3, test_metrics)
-        test_entry_list: List[DataEntry] = [test_entry_1, test_entry_2]
-        test_model.add_project(test_project)
+        self.__clear()
 
-        test_model.insert_datapoints(test_entry_list)
-        test_result: CFile = test_model.current_project.source_files[0]
+        self.test_model.add_project(self.test_project_1)
+        self.test_model.insert_datapoints(self.test_entry_list)
+
+        test_result: CFile = self.test_model.current_project.source_files[0]
         test_cfile: CFile = SourceFile("test_cfile")
-        test_cfile.data_entries.extend(test_entry_list)
+        test_cfile.data_entries.extend(self.test_entry_list)
 
         self.assertEqual(jsonpickle.encode(test_result), jsonpickle.encode(test_cfile))
 
     def test_add_project(self):
-        test_model: Model = Model()
-        test_project: Project = Project("", 123, "")
-        test_model.projects.append(test_project)
+        self.__clear()
+        self.test_model.add_project(self.test_project_1)
 
-        test_result: Model = Model()
-        test_result.add_project(test_project)
-
-        self.assertEqual(jsonpickle.encode(test_result.projects[0]), jsonpickle.encode(test_project))
+        self.assertEqual(jsonpickle.encode(self.test_model.projects[0]), jsonpickle.encode(self.test_project_1))
 
     def test_current_project(self):
-        test_model: Model = Model()
-        test_project: Project = Project("", 123, "")
-        test_model.projects.append(test_project)
+        self.__clear()
 
-        test_result: Model = Model()
-        test_result.add_project(Project("", 1234, ""))
-        test_result.add_project(test_project)
+        self.test_model.add_project(self.test_project_1)
+        self.test_model.add_project(self.test_project_2)
 
-        self.assertEqual(jsonpickle.encode(test_result.current_project), jsonpickle.encode(test_project))
+        self.assertEqual(jsonpickle.encode(self.test_model.current_project), jsonpickle.encode(self.test_project_2))
 
-    def test_get_sourcefile_by_name(self):
-        pass
+    def test_get_sourcefile_by_name_for_existing_cfile(self):
+        self.__clear()
+
+        self.test_model.add_project(self.test_project_1)
+        self.test_model.insert_datapoints(self.test_entry_list)
+        test_result = self.test_model.get_sourcefile_by_name("test_cfile")
+
+        test_cfile: CFile = SourceFile("test_cfile")
+        test_cfile.data_entries.extend(self.test_entry_list)
+        self.assertEqual(jsonpickle.encode(test_result), jsonpickle.encode(test_cfile))
+
+    def test_get_sourcefile_by_name_for_non_existing_cfile(self):
+        self.__clear()
+
+        self.test_model.add_project(self.test_project_1)
+
+        test_result = self.test_model.get_sourcefile_by_name("test_cfile")
+        test_cfile: CFile = SourceFile("test_cfile")
+
+        self.assertEqual(jsonpickle.encode(test_result), jsonpickle.encode(test_cfile))
 
     def test_get_project_name(self):
         pass
@@ -83,3 +91,11 @@ class TestModel(unittest.TestCase):
     def test_two_projects(self):
         pass
 
+    def __clear(self):
+        self.test_project_1 = Project("", 123, "")
+        self.test_project_2 = Project("", 1234, "")
+        self.test_metrics = [Metric(10, MetricName.CPU), Metric(10, MetricName.RAM)]
+        self.test_entry_1 = DataEntry("test_cfile", 2, self.test_metrics)
+        self.test_entry_2 = DataEntry("test_cfile", 3, self.test_metrics)
+        self.test_entry_list = [self.test_entry_1, self.test_entry_2]
+        self.test_model = Model()
