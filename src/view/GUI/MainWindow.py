873d96c10c1ec63648d14f09c549545c3d1625ac
Marie-Michelle Schneider <udixi@student.kit.edu>
2024-02-01 19:09:09 +0100

    corrected mistakes in visualize

--- src/view/GUI/MainWindow.py
+++ src/view/GUI/MainWindow.py
@@ -19,12 +19,13 @@ from src.model.core.CFile import CFile
 from src.model.core.CFileReadViewInterface import CFileReadViewInterface
 from src.view.GUI.Graph.Plot import Plot
 from src.model.core.MetricName import MetricName
+from src.view.GUI.Visuals.StatusBar import StatusBar
 
 
 class MainWindow(QMainWindow):
-    WINDOWSIZE1 = 800
-    WINDOWSIZE2 = 600
-    WINDOWTITLE = "CTI Engine"
+    WINDOWSIZE1: int = 800
+    WINDOWSIZE2: int = 600
+    WINDOWTITLE: str = "CTI Engine"
 
     def __init__(self):
         super().__init__()
@@ -43,7 +44,8 @@ class MainWindow(QMainWindow):
         self.top_frame_layout.addLayout(self.user_interaction_frame_layout)
 
         self.status_bar_frame_layout: QHBoxLayout = QVBoxLayout()
-        self.status_bar_frame_layout.addWidget(QCheckBox())  # Hier anstatt Checkbox die Status Bar hin
+        self.status_bar: StatusBar = StatusBar()
+        self.status_bar_frame_layout.addWidget(self.status_bar)
         self.top_frame_layout.addLayout(self.status_bar_frame_layout)
 
         self.widget_frame_layout: QVBoxLayout = QVBoxLayout()
@@ -81,16 +83,16 @@ class MainWindow(QMainWindow):
 
     def visualize(self, model):
         # Select spot for Displayables to be inserted into
-        self.table_widget.set_insertion_point(model.get_project_name())
+        self.table_widget.insertion_point = model.get_project_name()
 
         # Update TableWidget for each cfile
         cfile_list: List[CFileReadViewInterface] = model.get_cfiles()
         for cfile in cfile_list:
-            self.update_table(cfile)
+            self.table_widget.insert_values(self.__create_displayable(cfile))
 
         # Update other Widgets
         self.setup_connections()
-        """Statusbar muss hier geupdatet werden"""
+        self.status_bar.update_status("finished")
 
     # Possibly some mistakes here, needs testing
     def visualize_active(self, model: ModelReadViewInterface):
@@ -98,28 +100,31 @@ class MainWindow(QMainWindow):
         active_row: str = self.table_widget.insertion_point
         active_file: CFileReadViewInterface
         for cfile in model.get_cfiles():
-            active_file = self.get_hierachy(cfile, active_row)
+            active_file = self.__get_hierarchy(cfile, active_row)
             if active_file.get_name() == active_row:
                 break
 
-        # Update TableWidget for said file
-        self.update_table(active_file)
+        # Update TableWidget for header list for said file
+        cfile_list: List[CFileReadViewInterface] = active_file.get_headers()
+        for cfile in cfile_list:
+            self.table_widget.add_subrow(self.__create_displayable(cfile))
 
         # Update other Widgets
         self.setup_connections()
-        """Statusbar muss hier geupdatet werden"""
+        self.status_bar.update_status("finished")
 
     # Find cfile which started active mode
-    def get_hierachy(self, cfile: CFileReadViewInterface, active_row: str) -> CFileReadViewInterface:
+    def __get_hierarchy(self, cfile: CFileReadViewInterface, active_row: str) -> CFileReadViewInterface:
         if cfile.get_name() == active_row:
             return cfile
         elif not cfile.get_headers():
             return None
         for header in cfile.get_headers():
-            self.get_hierachy(header, active_row)
+            self.__get_hierarchy(header, active_row)
 
-    # Create Displayable for every cfile and insert into TableWidget
-    def update_table(self, cfile: CFileReadViewInterface):
+    # Create displayable
+    def __create_displayable(self, cfile: CFileReadViewInterface) -> Displayable:
+
         # Collect data for Displayable
         name: str = cfile.get_name()
         ram_peak: float = cfile.get_max(MetricName.RAM)
@@ -130,7 +135,7 @@ class MainWindow(QMainWindow):
         ram_y_values: List[float] = cfile.get_metrics(MetricName.RAM)
         cpu_y_values: List[float] = cfile.get_metrics(MetricName.CPU)
         runtime: List[float] = [cfile.get_total_time()]
-        color: str = self.generate_random_color()
+        color: str = self.__generate_random_color()
         ram_plot = Plot(name, color, x_values, ram_y_values)
         cpu_plot = Plot(name, color, x_values, cpu_y_values)
         runtime_plot = Plot(name, color, None, runtime)
@@ -140,12 +145,10 @@ class MainWindow(QMainWindow):
         for header in cfile.get_headers():
             headers.append(header.get_name())
 
-        # Create Displayable and insert into TableWidget
-        self.table_widget.insert_values(
-            Displayable(name, ram_plot, cpu_plot, runtime_plot, ram_peak, cpu_peak, headers))
+        return Displayable(name, ram_plot, cpu_plot, runtime_plot, ram_peak, cpu_peak, headers)
 
     # Generate Random Color for plot
-    def generate_random_color(self):
+    def __generate_random_color(self):
         random_color: str = "#{:06X}".format(random.randint(0, 0xFFFFFF))
         return random_color
 
