33be205bd280071bed5f220c86eb332aa4e349ee
KToroman <katarina@toroman.name>
2024-02-06 13:55:56 +0000

    fixed small bug with builder init

--- src/builder/header_builder/CompilingTool.py
+++ src/builder/header_builder/CompilingTool.py
@@ -13,21 +13,15 @@ class CompilingTool(BuilderInterface):
 
     DEFAULT_HEADER_DEPTH: int = 2
 
-    def __init__(self, source_file: SourceFile, path: str, header_depth: int = DEFAULT_HEADER_DEPTH) -> None:
+    def __init__(self, curr_project_dir: str, source_file: SourceFile, path: str, header_depth: int = DEFAULT_HEADER_DEPTH) -> None:
         self.source_file = source_file
-<<<<<<< HEAD
         self.__build_path = path
-        self.__file_builder = FileBuilder(self.source_file.compile_command)
+        self.__file_builder = FileBuilder(curr_project_dir = curr_project_dir, compile_command = self.source_file.compile_command, source_file_name = source_file.get_name(), build_path = path)
         self.__header_iterator = HeaderIterator(self.source_file, header_depth)
-=======
-        self.file_builder = FileBuilder(self.source_file.compile_command, source_file.get_name(), path)
-        self.header_iterator = HeaderIterator(self.source_file, header_depth)
->>>>>>> ca23c4aa036a738aad4aa4e8373050c0f570e449
 
     def build(self) -> bool:
         if not self.__header_iterator.has_next_header():
             return False
-<<<<<<< HEAD
         header: Header = self.__header_iterator.get_next_header()
         self.__file_builder.generate_source_file(header, self.__build_path)
         self.compile(header)
@@ -35,15 +29,6 @@ class CompilingTool(BuilderInterface):
 
     def compile(self, header: Header) -> None:
         command: str = self.__file_builder.get_compile_commmand(header)
-=======
-        header: Header = self.header_iterator.get_next_header()
-        self.file_builder.generate_source_file(header)
-        self.compile(header)
-        return self.header_iterator.has_next_header()
-
-    def compile(self, header: Header) -> None:
-        command: str = self.file_builder.get_compile_command(header)
->>>>>>> ca23c4aa036a738aad4aa4e8373050c0f570e449
         args: list[str] = shlex.split(command)
 
         proc: Popen = Popen(args)
