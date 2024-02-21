2e4981002b81d3b20e0349675a05ef6b03bf67f8
upufw_toroman <upufw@student.kit.edu>
2024-02-21 23:35:29 +0100

    added Qthread

--- src/view/GUI/AppUpdatesWorker.py
+++ src/view/GUI/AppUpdatesWorker.py
@@ -1,7 +1,29 @@
 from PyQt5.QtCore import QObject
 
+from threading import Event as TEvent
+from src.model.Model import Model
+#from src.view.GUI.MainWindow import MainWindow
+
 class AppUpdatesWorker(QObject):
-    visualize: bool = False
-    update_status: bool = False
-    def run(self):
-        pass
+    
+    def __init__(self, main_window):
+        super().__init__()
+        self.__main_window = main_window
+        self.shutdown_event = TEvent()
+
+    def start(self):
+        while not self.shutdown_event.is_set():
+            if self.__main_window.visualize_event.is_set():
+                if self.__main_window.model_queue.empty():
+                    raise BaseException("no model to visualize")
+                model: Model = self.__main_window.model_queue.get(True, 1.0)
+                self.__main_window.visualize(model)
+                #TODO what happens if timeout runs out -> model = None
+                if self.__main_window.model_queue.empty():
+                    self.__main_window.visualize_event.clear()
+            if not self.__main_window.status_queue.empty():
+                self.__main_window.update_statusbar(self.__main_window.status_queue.get(True, 1.0))
+            if not self.__main_window.error_queue.empty():
+                self.__main_window.deploy_error(self.__main_window.error_queue.get(True, 1.0))
+
+    
