c19535878d10178053636327bb2febd83e5aced2
upufw_toroman <upufw@student.kit.edu>
2024-02-21 19:42:02 +0100

    moved mp to main

--- src/__main__.py
+++ src/__main__.py
@@ -1,3 +1,4 @@
+from multiprocessing import Event, Queue
 import click
 
 from src.app.App import App
@@ -27,7 +28,7 @@ def start_active_measurement_command(self, source_file_name: str, path: str):
     app.run()
 
 def run_app():
-    app: App = App(True)
+    app: App = App(model_queue=model_queue,status_queue=status_queue, shutdown_event=shutdown_event, active_mode_event=active_mode_event,passive_mode_event=passive_mode_event, load_event=load_event, load_path_queue=load_path_queue, active_mode_queue=active_mode_queue, error_queue=error_queue, visualize_event=visualize_event, start_with_gui=True)
     app.run()
 
 mycommands.add_command(load_from_directory_command, "load")
@@ -35,4 +36,19 @@ mycommands.add_command(start_active_measurement_command, "actv")
 
 
 if __name__ == "__main__":
+    # Events for GUI
+    shutdown_event = Event()
+    active_mode_event = Event()
+    passive_mode_event = Event()
+    passive_mode_event.set()
+    load_event = Event()    
+    # Queues for GUI messages
+    load_path_queue: Queue = Queue(1)
+    active_mode_queue: Queue = Queue(1)
+    error_queue: Queue = Queue(5)
+    visualize_event = Event()
+    status_queue = Queue()
+    model_queue = Queue()
+
+
     mycommands()
