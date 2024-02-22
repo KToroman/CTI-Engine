8aed60b446fb8cd21b4052beb44e72f1cd1a198c
upufw_toroman <upufw@student.kit.edu>
2024-02-22 10:47:10 +0100

    process

--- src/__main__.py
+++ src/__main__.py
@@ -1,8 +1,11 @@
-from multiprocessing import Event, Queue, Manager
+from ast import main
+from multiprocessing import Event, Process, Queue, Manager
 from time import sleep
 import click
 
 from src.app.App import App
+from src.view.GUI.MainWindow import MainWindow
+from src.view.GUI.prepare_gui import prepare_gui
 
 @click.group(invoke_without_command=True)
 @click.pass_context
@@ -31,14 +34,9 @@ def start_active_measurement_command(self, source_file_name: str, path: str):
 
 
 def run_app():
-    app: App = App(model_queue=model_queue,status_queue=status_queue, 
-                   shutdown_event=shutdown_event, active_mode_event=active_mode_event, 
-                   passive_mode_event=passive_mode_event, load_event=load_event, 
-                   load_path_queue=load_path_queue, active_mode_queue=active_mode_queue, error_queue=error_queue, 
-                   visualize_event=visualize_event, start_with_gui=True)
     
-    sleep(10)
-    app.run()
+
+    app.run(main_window)
 
 mycommands.add_command(start_active_measurement_command, "actv")
 
@@ -58,6 +56,13 @@ if __name__ == "__main__":
     visualize_event = Event()
     status_queue = manager.Queue()
     model_queue = manager.Queue()
-
-
+    app: App = App(model_queue=model_queue,status_queue=status_queue, 
+                   shutdown_event=shutdown_event, active_mode_event=active_mode_event, 
+                   passive_mode_event=passive_mode_event, load_event=load_event, 
+                   load_path_queue=load_path_queue, active_mode_queue=active_mode_queue, error_queue=error_queue, 
+                   visualize_event=visualize_event, start_with_gui=True)
+    main_window: MainWindow = prepare_gui(app=app, visualize_event=visualize_event, status_queue=status_queue, 
+                                         model_queue=model_queue, error_queue=error_queue)
+    Process(target=main_window.execute).start()
+    print("process_started")
     mycommands()
