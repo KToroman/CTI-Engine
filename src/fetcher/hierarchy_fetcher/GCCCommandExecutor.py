8025a63d78b05ac8e93bd29f6b0035215bdf6651
Sebastian Lukas Sauer <uruoe@student.kit.edu>
2024-03-09 13:01:47 +0100

    Resolve "Hierarchy Fetcher is extremely slow"

--- src/fetcher/hierarchy_fetcher/GCCCommandExecutor.py
+++ src/fetcher/hierarchy_fetcher/GCCCommandExecutor.py
@@ -1,7 +1,5 @@
 import subprocess, shlex
 
-from io import StringIO
-
 class GCCCommandExecutor:
 
     def __init__(self) -> None:
@@ -13,6 +11,5 @@ class GCCCommandExecutor:
         completed_process: subprocess.CompletedProcess = subprocess.run(
             args=args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
         if completed_process.returncode != 0:
-            # print(completed_process.stderr)
             completed_process.check_returncode()
         return completed_process.stderr
