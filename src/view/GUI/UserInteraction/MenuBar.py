168245c069908a808a70382b031e0ce64c67e5e7
Jannik Brentel <uyioc@student.kit.edu>
2024-03-10 18:54:10 +0100

    Resolve "gui layout needs improvement"

--- src/view/GUI/UserInteraction/MenuBar.py
+++ src/view/GUI/UserInteraction/MenuBar.py
@@ -1,32 +1,77 @@
-from PyQt5.QtWidgets import QPushButton, QInputDialog
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QPushButton, QInputDialog, QTextEdit, QScrollArea, QWidget, QVBoxLayout
+import qtawesome as qta
 
 from src.view.AppRequestsInterface import AppRequestsInterface
 
 
 class MenuBar:
-    def __init__(self, menu_bar_layout, load_path_queue, cancel_event, restart_event):
+    def __init__(self, load_path_queue, cancel_event, restart_event):
 
         self.load_path_queue = load_path_queue
         self.cancel_event = cancel_event
         self.restart_event = restart_event
 
+        self.cancel_icon = qta.icon("ei.ban-circle")
+
         self.load_file_button: QPushButton = QPushButton("Load file")
         self.load_file_button.setStyleSheet("background-color: #61b3bf;")
         self.load_file_button.clicked.connect(lambda: self.show_input_dialog())
+
+        self.small_load_file_button: QPushButton = QPushButton()
+        self.small_load_file_button.setStyleSheet("background-color: #61b3bf;")
+        self.small_load_file_button.clicked.connect(lambda: self.show_input_dialog())
 
         self.pause_resume_button: QPushButton = QPushButton("Restart")
         self.pause_resume_button.clicked.connect(lambda: self.restart_event.set())
         self.pause_resume_button.setStyleSheet("background-color: #61b3bf;")
 
+        self.small_pause_resume_button: QPushButton = QPushButton("icon")
+        self.small_pause_resume_button.clicked.connect(lambda: self.restart_event.set())
+        self.small_pause_resume_button.setStyleSheet("background-color: #61b3bf;")
+
         self.cancel_button: QPushButton = QPushButton("Cancel")
+        self.cancel_button.setIcon(self.cancel_icon)
         self.cancel_button.setStyleSheet("background-color: #61b3bf;")
         self.cancel_button.clicked.connect(lambda: self.cancel_event.set())
 
-        menu_bar_layout.addWidget(self.load_file_button)
-        menu_bar_layout.addWidget(self.pause_resume_button)
-        menu_bar_layout.addWidget(self.cancel_button)
+        self.small_cancel_button: QPushButton = QPushButton()
+        self.small_cancel_button.setIcon(self.cancel_icon)
+        self.small_cancel_button.setStyleSheet("background-color: #61b3bf;")
+        self.small_cancel_button.clicked.connect(lambda: self.cancel_event.set())
+
+        self.scroll_bar = QScrollArea()
+        self.scroll_bar.setWidgetResizable(True)
+        self.scroll_bar.setHidden(True)
+        self.scroll_bar.setMaximumSize(135, 500)
+        self.scroll_bar.setMinimumHeight(200)
+        self.scroll_widget = QWidget()
+        self.scroll_layout = QVBoxLayout(self.scroll_widget)
+        for i in range(100):
+            a = QPushButton("Deine Mom")
+            self.scroll_layout.addWidget(a)
+        self.scroll_bar.setWidget(self.scroll_widget)
+
+        self.icon = qta.icon("ei.align-justify")
+        self.scroll_button = QPushButton("All Projects")
+        self.scroll_button.setIcon(self.icon)
+        self.scroll_button.setStyleSheet("background-color: #61b3bf;")
+        self.scroll_button.setCheckable(True)
+        self.scroll_button.toggled.connect(lambda: self.toggle_scrollbar())
+
+
 
     def show_input_dialog(self):
         text, ok = QInputDialog.getText(None, "File input", 'Enter file name:')
         if ok:
             self.load_path_queue.put(text)
+
+    def toggle_scrollbar(self):
+        # Überprüfen, ob die Scrollbar angezeigt wird oder nicht, und umschalten
+        if self.scroll_bar.isHidden():
+            self.scroll_bar.setHidden(False)
+        else:
+            self.scroll_bar.setHidden(True)
+
+        # Das Widget neu zeichnen, um die Änderungen anzuzeigen
+        self.scroll_bar.update()
--- src/view/GUI/UserInteraction/MetricBar.py
+++ src/view/GUI/UserInteraction/MetricBar.py
@@ -2,13 +2,10 @@ from PyQt5.QtWidgets import QPushButton
 
 
 class MetricBar:
-    def __init__(self, metric_frame_layout):
+    def __init__(self):
         self.cpu_button = QPushButton("CPU")
         self.cpu_button.setStyleSheet("background-color: #4095a1;")
         self.ram_button = QPushButton("RAM")
         self.ram_button.setStyleSheet("background-color: #4095a1;")
         self.runtime_button = QPushButton("Runtime")
         self.runtime_button.setStyleSheet("background-color: #4095a1;")
-        metric_frame_layout.addWidget(self.ram_button)
-        metric_frame_layout.addWidget(self.cpu_button)
-        metric_frame_layout.addWidget(self.runtime_button)
