564fd3275aa6091dcb7d8754850dbe1e12fb4dfa
upufw_toroman <upufw@student.kit.edu>
2024-03-15 16:12:20 +0100

    changed saveinterface

--- src/saving/SaveInterface.py
+++ src/saving/SaveInterface.py
@@ -1,9 +1,10 @@
-from typing import Protocol
+from typing import List, Protocol
+from src.model.DataBaseEntry import DataBaseEntry
 
 from src.model.core.Project import Project
 
 
 class SaveInterface(Protocol):
     """SaveInterface is used to save projects."""
-    def save_project(self, project: Project):
+    def save_project(self, project: Project, delta: List[DataBaseEntry]):
         """Saves the given project into a file."""
