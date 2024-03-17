395d3ddf0c9b934abb951aa7a15398ce672e52c2
KToroman <katarina@toroman.name>
2024-03-17 22:21:30 +0000

    debug

--- src/fetcher/file_fetcher/FileLoader.py
+++ src/fetcher/file_fetcher/FileLoader.py
@@ -105,6 +105,7 @@ class FileLoader(FetcherInterface):
             return new_header
         else:
             new_sourcefile: SourceFile = SourceFile(path)
+            print("new sourcefile path: "+new_sourcefile.path)
             project.source_files.append(new_sourcefile)
             self.__all_cfiles.update({path: new_sourcefile})
             print("[FileLoader]     found new sourcefile to save")
